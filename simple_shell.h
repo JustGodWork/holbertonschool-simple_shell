3beb69f7e11d0468845485bc77df96da514de42a
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-12-17 19:10:21 +0100

    refractor of child process handling

--- simple_shell.h
+++ simple_shell.h
@@ -2,10 +2,12 @@
 #define SIMPLE_SHELL_H
 
 #define EXIT_CHILD 3 /* Exit status for child process */
-#define DEBUG 1 /* 0 = false, 1 = true */
+#define DEBUG 0 /* 0 = false, 1 = true */
 
 #include "libraries.h"
 #include "debug.h"
+#include "listeners.h"
+#include "process.h"
 
 /* exit_handler.c */
 int exit_handler(char *command);
